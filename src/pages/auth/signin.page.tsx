e59dca6a5684629de2ef7134c5574a034f3f1f27
reubenjh <rjharcourt@gmail.com>
2022-10-09 11:50:28 +1300

    Improve error message

--- src/pages/auth/signin.page.tsx
+++ src/pages/auth/signin.page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { NextPage } from 'next/types';
@@ -17,8 +18,15 @@ const Signin: NextPage = () => {
     <div className="t.flex t.flex-col t.justify-center t.h-screen t.items-center">
       <div className="t.container t.mx-auto t.py-8 t.px-4 t.flex t.flex-col t.justify-center t.items-center">
         {error && (
-          <div className="t.p-4 t.mb-4 t.bg-blue t.rounded-lg">
-            Uh oh: {error}
+          <div className="t.p-6 t.mb-4 t.bg-blue t.rounded-lg t.max-w-sm">
+            <div>Uh oh. Something went wrong.</div>
+
+            <div className="t.mt-4">
+              This may mean that you've logged in to your bloodrush account
+              before using a different provider than the one you just tried (you
+              can only use 1 per email address). Please try the correct
+              provider, otherwise sign in with email.
+            </div>
           </div>
         )}
         <Logo className="t.mb-6 t.text-center" />
