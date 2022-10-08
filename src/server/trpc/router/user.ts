e44acf8d4272669f7b493772d826b9b2b0b36994
reubenjh <rjharcourt@gmail.com>
2022-10-08 23:17:40 +1300

    Comment out redundant trpc router

--- src/server/trpc/router/user.ts
+++ src/server/trpc/router/user.ts
@@ -3,33 +3,33 @@ import { z } from 'zod';
 import { t } from '../trpc';
 
 export const userRouter = t.router({
-  get: t.procedure
-    .input(z.object({ remote_id: z.string().nullish() }))
-    .query(({ ctx, input }) => {
-      if (!input.remote_id) {
-        throw new TRPCError({
-          message: 'Please provide a remote_id to query on',
-          code: 'BAD_REQUEST',
-        });
-      }
-      return ctx.prisma.user.findUniqueOrThrow({
-        where: { remote_id: input.remote_id },
-      });
-    }),
-  create: t.procedure
-    .input(
-      z.object({
-        remote_id: z.string(),
-        name: z.string(),
-        email: z.string(),
-      }),
-    )
-    .mutation(({ ctx, input }) => {
-      console.log('in create');
-      return ctx.prisma.user.create({
-        data: {
-          ...input,
-        },
-      });
-    }),
+  // get: t.procedure
+  //   .input(z.object({ remote_id: z.string().nullish() }))
+  //   .query(({ ctx, input }) => {
+  //     if (!input.remote_id) {
+  //       throw new TRPCError({
+  //         message: 'Please provide a remote_id to query on',
+  //         code: 'BAD_REQUEST',
+  //       });
+  //     }
+  //     return ctx.prisma.user.findUniqueOrThrow({
+  //       where: { remote_id: input.remote_id },
+  //     });
+  //   }),
+  // create: t.procedure
+  //   .input(
+  //     z.object({
+  //       remote_id: z.string(),
+  //       name: z.string(),
+  //       email: z.string(),
+  //     }),
+  //   )
+  //   .mutation(({ ctx, input }) => {
+  //     console.log('in create');
+  //     return ctx.prisma.user.create({
+  //       data: {
+  //         ...input,
+  //       },
+  //     });
+  //   }),
 });
